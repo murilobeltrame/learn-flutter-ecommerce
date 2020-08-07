c026a0bff0f31dbde38e6ec0616c5bacd0b1b319
Murilo Beltrame <murilobeltrame@gmail.com>
2020-08-07 01:00:49 -0300

    added product screen

--- lib/tiles/product_tile.dart
+++ lib/tiles/product_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/datas/product_data.dart';
+import 'package:loja_virtual/screens/product_screen.dart';
 
 class ProductTile extends StatelessWidget {
 
@@ -10,6 +11,82 @@ class ProductTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => ProductScreen(data))
+        );
+      },
+      child: Card(
+        child: type == 'grid' ?
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 0.8,
+                child: Image.network(
+                  data.images[0],
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        data.title,
+                        style: TextStyle(fontWeight: FontWeight.w500),
+                      ),
+                      Text(
+                        'R\$ ${data.price.toStringAsFixed(2)}',
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ):
+          Row(
+            children: <Widget>[
+              Flexible(
+                flex: 1,
+                child: Image.network(
+                  data.images[0],
+                  fit: BoxFit.cover,
+                  height: 250.0,
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        data.title,
+                        style: TextStyle(fontWeight: FontWeight.w500),
+                      ),
+                      Text(
+                        'R\$ ${data.price.toStringAsFixed(2)}',
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+      ),
+    );
   }
 }
